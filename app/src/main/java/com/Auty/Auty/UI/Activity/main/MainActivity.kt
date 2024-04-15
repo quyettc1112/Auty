2dd9662b9fc1afdec31d1fe7bb863414f43a771e
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-04-15 19:12:57 +0700

    Update SplashActivity

--- app/src/main/java/com/Auty/Auty/UI/Activity/main/MainActivity.kt
+++ app/src/main/java/com/Auty/Auty/UI/Activity/main/MainActivity.kt
@@ -14,10 +14,10 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
 }
