f94efc5d69565bb457e66bcad772aa3b65dd8c1c
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-04-15 19:12:49 +0700

    Update SplashActivity

--- app/src/main/java/com/Auty/Auty/UI/Activity/splash/SplashActivity.kt
+++ app/src/main/java/com/Auty/Auty/UI/Activity/splash/SplashActivity.kt
@@ -1,11 +1,15 @@
 package com.Auty.Auty.UI.Activity.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.Auty.Auty.R
 import com.Auty.Auty.AppConfig.BaseConfig.BaseActivity
+import com.Auty.Auty.UI.Activity.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,5 +23,21 @@ class SplashActivity : BaseActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+       IntentMainActivity()
     }
+
+    private fun loadUserData() {
+
+    }
+
+    private fun IntentMainActivity() {
+        val  delayMillis: Long = 1000 // 1,25  second
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, delayMillis)
+    }
+
+
+
 }
