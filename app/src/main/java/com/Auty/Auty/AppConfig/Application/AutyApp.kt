c17bcf82cc503f58826a1ae24de26913aaab7601
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-04-15 21:59:11 +0700

    Update SplashActivity

--- app/src/main/java/com/Auty/Auty/AppConfig/Application/AutyApp.kt
+++ app/src/main/java/com/Auty/Auty/AppConfig/Application/AutyApp.kt
@@ -1,8 +1,13 @@
 package com.Auty.Auty.AppConfig.Application
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AutyApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
