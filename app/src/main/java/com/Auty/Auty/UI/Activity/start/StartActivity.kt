ae76f4dde437d39d2e11487f33dea683d5ba8d01
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-04-20 23:16:21 +0700

    Update Started UI

--- app/src/main/java/com/Auty/Auty/UI/Activity/start/StartActivity.kt
+++ app/src/main/java/com/Auty/Auty/UI/Activity/start/StartActivity.kt
@@ -1,6 +1,7 @@
 package com.Auty.Auty.UI.Activity.start
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,7 +20,7 @@ class StartActivity : BaseActivity() {
         enableEdgeToEdge()
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
