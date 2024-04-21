79b11d2d3eff56de4184c5021642166fd6ec7876
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-04-21 23:37:53 +0700

    Update Login UI

---
+++ app/src/main/java/com/Auty/Auty/UI/Activity/authen/RegisterActivity.kt
@@ -0,0 +1,24 @@
+package com.Auty.Auty.UI.Activity.authen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.Auty.Auty.AppConfig.BaseConfig.BaseActivity
+import com.Auty.Auty.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RegisterActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
