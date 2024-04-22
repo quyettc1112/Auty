bbc71fe813065635dc60d25645a3aceacb90a237
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-04-22 15:02:59 +0700

    Update some animation

--- app/src/main/java/com/Auty/Auty/AppConfig/Constant/Constant.kt
+++ app/src/main/java/com/Auty/Auty/AppConfig/Constant/Constant.kt
@@ -1,8 +1,13 @@
 package com.Auty.Auty.AppConfig.Constant
 
+import android.view.animation.AnimationUtils
+import com.Auty.Auty.R
+
 class Constant {
     companion object {
         const val DEFAULT_ERROR_MESSAGE: String = "An error occurred"
+
+
     }
 
 
