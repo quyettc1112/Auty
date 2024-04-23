67ef5986074ce59c3bffeb2a97733bb91be6513e
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-04-23 17:20:12 +0700

    Update Bottom Nav navigation

--- app/src/main/java/com/Auty/Auty/UI/Activity/main/MainActivity.kt
+++ app/src/main/java/com/Auty/Auty/UI/Activity/main/MainActivity.kt
@@ -9,25 +9,17 @@ import androidx.core.view.WindowInsetsCompat
 import com.Auty.Auty.AppConfig.BaseConfig.BaseActivity
 import com.Auty.Auty.R
 import com.Auty.Auty.UI.Activity.authen.LoginActivity
+import com.Auty.Auty.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
-        val test = findViewById<Button>(R.id.test_button).let {
-            it.setOnClickListener {
-                Intent(this, LoginActivity::class.java).also {
-                    startActivity(it)
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
