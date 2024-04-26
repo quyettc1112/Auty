f07a8ef38c70b539695ea12f89141eddc124717d
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-04-27 02:18:30 +0700

    Done UI Home

--- app/src/main/java/com/Auty/Auty/AppConfig/Constant/Constant.kt
+++ app/src/main/java/com/Auty/Auty/AppConfig/Constant/Constant.kt
@@ -1,6 +1,7 @@
 package com.Auty.Auty.AppConfig.Constant
 
 import android.view.animation.AnimationUtils
+import com.Auty.Auty.Common.CourseMainAdapter
 import com.Auty.Auty.R
 
 class Constant {
@@ -20,6 +21,65 @@ class Constant {
         const val DEFAULT_PRIMARY_COLOR = "#17A4FE"
         const val DEFAULT_PRIMARY_COLOR_ACTIVE = "#85C6EF"
 
+
+        fun getListCourse(): List<CourseMainAdapter.Course> {
+            return listOf(
+                CourseMainAdapter.Course(
+                    title = "Cách nhận biết trẻ bị tự kỷ",
+                    image = R.drawable.ic_materaial_course,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                ),
+
+                CourseMainAdapter.Course(
+                    title = "Biểu hiện của trẻ bị tự kỷ theo các mức độ",
+                    image = R.drawable.ic_materaial_course,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                ),
+
+                CourseMainAdapter.Course(
+                    title = "Cách nhận biết trẻ bị tự kỷ",
+                    image = R.drawable.ic_material_recommend,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                ),
+
+                CourseMainAdapter.Course(
+                    title = "Độ tuổi dễ bị tổn thương sâu sắc về tình cảm",
+                    image = R.drawable.ic_material_aytism,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                ),
+
+                CourseMainAdapter.Course(
+                    title = "Cách nhận biết trẻ bị tự kỷ",
+                    image = R.drawable.ic_materaial_course,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                ),
+                CourseMainAdapter.Course(
+                    title = "Cách nhận biết trẻ bị tự kỷ",
+                    image = R.drawable.ic_materaial_course,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                )
+            )
+
+
+        }
 
     }
 
